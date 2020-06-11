970804d8312b407403a9fa90b8d1e4a7dfd9c5b7
AamnaAli <g00075371@aus.edu>
2020-06-11 15:18:17 +0400

    multipication1 implemented

---
+++ Calculations.java
@@ -0,0 +1,24 @@
+public class Calculations {
+
+	public static void main(String[] args) {
+		int x = 2;
+		int y = 1;
+		
+		//sum of x and y 
+		int sum = x+y;
+		System.out.println("The sum is= "+ sum);
+		
+		//member_B add the feature of subtracting x from y and print the result
+		int sub = y-x;
+                System.out.println("The subtraction is  = " + sub);
+                
+		//member_C add the feature of dividing x by y and print the result
+                int div = x/y;
+                System.out.println("The quotient is = " + div);
+		
+		//member_D add the feature of multiplying x by y and print the result
+		
+		
+	}
+
+}
